4620fa70e1799dd58c84ac8bea477318dd259cf6
sofyashumskikh <91876971+sofyashumskikh@users.noreply.github.com>
2024-12-14 22:21:45 +0300

    demo pages

--- DatingApplication/frontend/src/pages/AuthPage.vue
+++ DatingApplication/frontend/src/pages/AuthPage.vue
@@ -1,9 +1,39 @@
 <template>
-  <q-page padding>
-    <!-- content -->
-  </q-page>
-</template>
-
-<script setup>
-//
-</script>
+    <div class="login-page">
+      <div class="text-center">
+        <h2>Авторизация</h2>
+      </div>
+      <div class="fixed-center" style="width: 450px;">
+        <q-card class="my-card">
+          <q-card-section>
+            <q-input outlined label="Email" v-model="email" />
+            <br>
+            <q-input outlined label="Password" v-model="password" type="password"/>
+            <br>
+            <q-btn outline rounded color="primary" label="Login" @click="login"/>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+        setup() {
+            return {
+                email: '',
+                password: '',
+            };
+        },
+        methods: {
+            login() {
+            },
+        },  
+    };
+  </script>
+  
+  <style lang="sass">
+  .my-card
+    width: 100%
+    max-width: 500px 
+  </style>

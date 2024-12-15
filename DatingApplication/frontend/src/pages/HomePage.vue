c24bb4bfb0988fa8e57f9832a818a15ce57de2f7
sofyashumskikh <91876971+sofyashumskikh@users.noreply.github.com>
2024-12-15 19:56:18 +0300

    Add files via upload

--- DatingApplication/frontend/src/pages/HomePage.vue
+++ DatingApplication/frontend/src/pages/HomePage.vue
@@ -1,9 +1,37 @@
 <template>
-  <q-page padding>
-    <!-- content -->
-  </q-page>
-</template>
-
-<script setup>
-//
-</script>
+    <div class="text-center">
+      <p>some text about app</p>
+    </div>
+    <div class="home">
+        <q-btn outline rounded color="primary" label="Войти" @click="enter" 
+        style="width: 200px; height: 20px;"/>
+        <br>
+        <q-btn outline rounded color="primary" label="Зарегистрироваться" @click="enter" 
+        style="margin-top: 20px; width: 200px; height: 20px;"/>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      methods: {
+        enter() {
+        },
+      },  
+    };
+  </script>
+  
+  <style scoped>
+    .home {
+      margin: 0;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      margin-right: -50%;
+      transform: translate(-50%, -50%);
+    }
+    p {
+      text-align: center;
+      font-size: xx-large;
+      color: grey;
+    }
+  </style>
--- DatingApplication/frontend/src/pages/SearchPage.vue
+++ DatingApplication/frontend/src/pages/SearchPage.vue
@@ -1,9 +1,32 @@
 <template>
-  <q-page padding>
-    <!-- content -->
-  </q-page>
-</template>
-
-<script setup>
-//
-</script>
+    <div class="text-center">
+      <h2>... кандидаты ...</h2>
+    </div>
+    <div class="q-pa-md">
+      <div class="q-col-gutter-md row items-start">
+        <div class="col-6">
+          <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" style="width: 450px; height: 450px">
+            <div class="absolute-bottom text-subtitle1 text-center">
+              Caption
+            </div>
+          </q-img>
+        </div>
+        <div class="col-6">
+          <q-card style="width: 450px; height: 450px">
+            <div class="text-center">text text text</div> 
+          </q-card>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <style scoped>
+    .q-pa-md {
+      margin: 0;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      margin-right: -50%;
+      transform: translate(-50%, -50%)
+    }
+  </style>

0fcfe44999ee94a11680022950b65acb305ff0f3
sofyashumskikh <91876971+sofyashumskikh@users.noreply.github.com>
2024-12-21 17:35:44 +0300

    Add files via upload

--- DatingApplication/frontend/src/pages/SearchPage.vue
+++ DatingApplication/frontend/src/pages/SearchPage.vue
@@ -4,25 +4,18 @@
   </div>
   <div class="checkmark-container">
     <q-icon name="check" color="green" size="6em" />
-    <!-- <q-icon name="close" color="red" size="6em"/> -->
   </div>
-  <!-- <div class="sort-container">
-    <q-select v-model="selectedSort" :options="sortOptions" label="Сортировать по" style="width: 200px; right: 0%;"
-      class="sort-menu" />
-  </div> -->
   <div class="q-pa-md">
     <div class="q-col-gutter-md row items-start">
       <div class="col-6">
         <q-carousel animated v-model="slide" arrows navigation infinite style="width: 450px; height: 450px;">
-          <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
-          <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
-          <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
+          <q-carousel-slide v-for="profile in profiles" :key="profile.id" :name="profile.id" :img-src="profile.image" />
         </q-carousel>
       </div>
       <div class="col-6">
-        <q-card style="width: 450px; height: 450px">
-          <div class="text-center">
-            <p style="font-size: xx-large;">text<br>text</p>
+        <q-card class="profile-card">
+          <div class="text-center profile-text">
+            <p style="font-size: xx-large;">{{ currentProfile.text }}</p>
           </div>
         </q-card>
       </div>
@@ -32,25 +25,56 @@
       <q-btn outline rounded color="primary" label="DISLIKE" @click="dislike" />
     </div>
     <br>
-    <q-btn outline rounded color="primary" label="ПОЖАЛОВАТЬСЯ" @click="complaint" style="left: 42%;" />
+    <q-btn outline rounded color="primary" label="ПОЖАЛОВАТЬСЯ" @click="openComplaintDialog" style="left: 42%;" />
   </div>
+  <q-dialog v-model="showComplaintDialog">
+    <q-card style="width: 300px;">
+      <q-card-section>
+        <div class="text-h6">Опишите вашу жалобу</div>
+        <q-input outlined v-model="complaintText" type="textarea" />
+      </q-card-section>
+      <q-card-actions>
+        <q-btn label="Отмена" outline rounded color="primary" v-close-popup style="margin-left: 50px;" />
+        <q-btn label="Отправить" outline rounded color="negative" @click="sendComplaint" style="margin-left: 25px;" />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script>
 import { useRouter } from "vue-router";
 import axios from "axios";
-import { ref } from "vue";
+import { ref, onMounted, computed } from "vue";
+
 
 export default {
   setup() {
 
     const router = useRouter();
-    // const selectedSort = ref(null);
-    // const sortOptions = ref([
-    //   { label: "По умолчанию", value: "default" },
-    //   { label: "По возрасту", value: "age" },
-    //   { label: "По стране", value: "country" }
-    // ]);
+    const slide = ref(1);
+    const showComplaintDialog = ref(false);
+    const complaintText = ref("");
+    const userId = 1;
+    const profiles = ref([]);
+
+    const currentProfile = computed(() => {
+      const found = profiles.value.find(p => p.id === slide.value);
+      return found || { text: '' };
+    });
+
+    const getProfiles = async () => {
+      try {
+        const response = await axios.get("/api/profiles");
+        profiles.value = response.data;
+        if (profiles.value.length > 0) {
+          slide.value = profiles.value[0].id;
+        }
+      } catch (error) {
+        console.error("Failed to fetch profiles", error);
+      }
+    };
+
+    onMounted(getProfiles);
 
     axios.interceptors.response.use(
       (response) => response,
@@ -62,17 +86,69 @@ export default {
       }
     );
 
-    const like = () => { };
-    const dislike = () => { };
-    const complaint = () => { };
+    const openComplaintDialog = () => {
+      showComplaintDialog.value = true;
+      complaintText.value = "";
+    };
+
+    const like = async () => {
+      try {
+        const response = await axios.post("/api/like", {
+          userId: userId,
+        });
+        if (response.status === 200) {
+          console.log("Like was successful");
+        } else {
+          console.error("Like failed, server returned:", response.status);
+        }
+      } catch (error) {
+        console.error("Error during like action:", error);
+      }
+    };
+
+    const dislike = async () => {
+      try {
+        const response = await axios.post("/api/dislike", {
+          userId: userId,
+        });
+        if (response.status === 200) {
+          console.log("Dislike was successful");
+        } else {
+          console.error("Dislike failed, server returned:", response.status);
+        }
+      } catch (error) {
+        console.error("Error during dislike action:", error);
+      }
+    };
+
+    const sendComplaint = async () => {
+      try {
+        const response = await axios.post("/api/complaint", {
+          userId: userId,
+          text: complaintText.value
+        });
+        if (response.status === 200) {
+          console.log("Complaint was successful");
+          showComplaintDialog.value = false;
+        } else {
+          console.error("Complaint failed, server returned:", response.status);
+        }
+      } catch (error) {
+        console.error("Error during complaint action:", error);
+      }
+    };
+
 
     return {
-      slide: ref(1),
+      slide,
       like,
       dislike,
-      complaint,
-      //selectedSort,
-      //sortOptions,
+      profiles,
+      currentProfile,
+      complaintText,
+      showComplaintDialog,
+      openComplaintDialog,
+      sendComplaint,
     };
   },
 }
@@ -107,20 +183,17 @@ export default {
   top: 5%;
   right: 7%;
 }
-/*
-.sort-container {
-  position: relative;
+
+.profile-card {
+  width: 450px;
+  height: 450px;
   display: flex;
-  justify-content: flex-end;
   align-items: center;
-  padding-right: 5%;
+  justify-content: center;
+  box-sizing: border-box;
 }
 
-.sort-menu {
-  margin-top: 20px;
+.profile-text {
+  padding: 16px;
 }
-*/
 </style>
-
-<!-- TODO :
-добавить всплывающие уведомления  -->
